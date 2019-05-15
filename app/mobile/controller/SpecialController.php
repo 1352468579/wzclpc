1cd9f6e9b621599067a45923be38b4454d9bbe2e
wang <279052572@qq.com>
2019-05-15 14:34:37 +0800

    banqi zhuanti

--- app/mobile/controller/SpecialController.php
+++ app/mobile/controller/SpecialController.php
@@ -9,9 +9,28 @@ namespace app\mobile\controller;
 
 use cmf\controller\WebBaseController;
 use Think\Exception;
+use app\portal\model\PortalCategoryModel;
 header('Content-type:text/html;charset=utf-8;');
 class SpecialController extends WebBaseController
 {
+
+    const GRADE_ID = 23;
+    /************
+     *----==== 初始化函数 ====----
+     *Created by w 2019/4/21 15:31
+     ************/
+    public function _initialize()
+    {
+        parent::_initialize();
+        $portalCategoryModel = new PortalCategoryModel();
+        $where = ['delete_time' => 0,'path'=>['Like','0-'.self::GRADE_ID.'%'],'parent_id'=>self::GRADE_ID];
+        $categories = $portalCategoryModel->order("list_order ASC")->where($where)->select()->toArray();
+        $this->assign('grade', $categories);
+        if (!session('my-token')) {
+            session('my-token',md5(time()));
+        }
+        $this->assign('token', session('my-token'));
+    }
     /************
     *----==== 春季班 ====----
     *Created by w 2019/4/29 14:56
@@ -25,6 +44,20 @@ class SpecialController extends WebBaseController
      ************/
     public function summer(){
         return $this->fetch();
+    }
+    /************
+     *----==== 把脉期中 ====----
+     *Created by w 2019/4/29 14:56
+     ************/
+    public function midterm(){
+        return $this->fetch();
+    }
+    /************
+     *----==== 把脉期中PC ====----
+     *Created by w 2019/4/29 14:56
+     ************/
+    public function midtermPC(){
+        return $this->fetch("specialPc/midterm");
     }
     /************
      *----==== 春季班PC ====----
